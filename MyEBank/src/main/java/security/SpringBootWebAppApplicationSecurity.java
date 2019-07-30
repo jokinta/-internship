4bc7fa73343497967e8e078308547f5482c0fb90
Jovan Ivanov <36512938+jokinta@users.noreply.github.com>
2019-07-30 13:33:51 +0300

    Add files via upload

---
+++ MyEBank/src/main/java/security/SpringBootWebAppApplicationSecurity.java
@@ -0,0 +1,32 @@
+package security;
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@ComponentScan(basePackages= {"sypks_accounts.rest_controller"})
+@ComponentScan(basePackages= {"sypks_accounts.rest_services"})
+@ComponentScan(basePackages= {"sypks_user_login.rest_controllers"})
+@ComponentScan(basePackages= {"sypks_user_login.rest_services"})
+@ComponentScan(basePackages= {"sypks_transactions.services"})
+@ComponentScan(basePackages= {"sypks_transactions.controllers"})
+@RestController
+@EnableAuthorizationServer
+@SpringBootApplication
+public class SpringBootWebAppApplicationSecurity {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplicationSecurity.class, args);
+	}
+
+	@RequestMapping("/validateUser")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+}
